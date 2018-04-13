ccd2a897193982021c40bdc1f9df9ffb51a5a7e6
fufukha <wafakhalid29@gmail.com>
2018-04-13 19:46:14 -0400

    moved index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/app'; 
+import './css/reset.css';
 import './sass/main.scss';
 
 function component() {
